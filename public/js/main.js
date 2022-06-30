a1e80891a5dee969acdcb6f7477ca627f29cea4e
Satyam Bajpai <bajpai1973satyam@gmail.com>
2022-07-01 01:19:03 +0530

    added dotenv file for api key

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+// require('dotenv').config();
+// var requirejs = require('requirejs');
+
+
+// requirejs.config({
+//     //Pass the top-level main.js/index.js require
+//     //function to requirejs so that node modules
+//     //are loaded relative to the top-level JS file.
+//     env: "node_modules/env/config.js"
+// });
+
+// requirejs(['foo', 'bar'],
+//     function(foo, bar) {
+//         //foo and bar are loaded according to requirejs
+//         //config, but if not found, then node's require
+//         //is used to load the module.
+//     });
 const submitBtn = document.getElementById("submitBtn");
 const cityName = document.getElementById("cityName");
 const city_name = document.getElementById("city_name");
@@ -6,85 +23,92 @@ const temp_status = document.getElementById("temp_status")
 const dataHide = document.querySelector(".middle_layer");
 const today = document.getElementById("day");
 const today_date = document.getElementById("today_date");
-const getInfo = async (e) => {
-  e.preventDefault();
-  let cityVal = cityName.value;
-  if (cityVal === "") {
-    city_name.innerText = "Please write the name before you search";
-    dataHide.classList.add("data_hide");
-  } else {
+// console.log(process.env.API_KEY)
+const getInfo = async(e) => {
+    e.preventDefault();
+    let cityVal = cityName.value;
+    let data;
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=6ae21b337bd2eb15be09c286bda68ba9`;
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      const arrData = [data];
-      temp_real_val.innerText = arrData[0].main.temp;
-      //temp_status.innerText = arrData[0].weather[0].main;
-      city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
 
-      const tempStatus = arrData[0].weather[0].main;
-      if (tempStatus == "Clear") {
-        temp_status.innerHTML =
-          "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
-      } else if (tempStatus == "Clouds") {
-        temp_status.innerHTML =
-          "<i class='fas  fa-cloud' style='color: #009ad8;'></i>";
-      } else if (tempStatus == "Rainy") {
-        temp_status.innerHTML =
-          "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
-      } else if (tempStatus == "Mist") {
-        temp_status.innerHTML =
-          "<i class='fas fa-smog' style='color: #d4d4d4;'></i>";
-      } else if (tempStatus == "Rain") {
-        temp_status.innerHTML = " <i class='fas fa-cloud-rain' style='color: #009ad8;'></i>"
-      }
-
-      dataHide.classList.remove("data_hide");
-    } catch (err) {
-      city_name.innerText = "Please enter the city name properly";
-      dataHide.classList.add("data_hide");
+        let getData = await fetch(`/api/weather/${cityVal}`)
+        let response = await getData.json();
+        data = JSON.parse(response);
+    } catch (error) {
+        console.log(error);
     }
-  }
+
+    if (cityVal === "") {
+        city_name.innerText = "Please write the name before you search";
+        dataHide.classList.add("data_hide");
+    } else {
+        try {
+            const arrData = [data];
+            temp_real_val.innerText = arrData[0].main.temp;
+            //temp_status.innerText = arrData[0].weather[0].main;
+            city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+
+            const tempStatus = arrData[0].weather[0].main;
+            if (tempStatus == "Clear") {
+                temp_status.innerHTML =
+                    "<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+            } else if (tempStatus == "Clouds") {
+                temp_status.innerHTML =
+                    "<i class='fas  fa-cloud' style='color: #009ad8;'></i>";
+            } else if (tempStatus == "Rainy") {
+                temp_status.innerHTML =
+                    "<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+            } else if (tempStatus == "Mist") {
+                temp_status.innerHTML =
+                    "<i class='fas fa-smog' style='color: #d4d4d4;'></i>";
+            } else if (tempStatus == "Rain") {
+                temp_status.innerHTML = " <i class='fas fa-cloud-rain' style='color: #009ad8;'></i>"
+            }
+
+            dataHide.classList.remove("data_hide");
+        } catch (err) {
+            city_name.innerText = "Please enter the city name properly";
+            dataHide.classList.add("data_hide");
+        }
+    }
 }
 submitBtn.addEventListener("click", getInfo);
 
 
 
 const getCurrentDay = () => {
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-  let currentTime = new Date();
-  let day = weekday[currentTime.getDay()];
-  today.innerText = day;
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    let currentTime = new Date();
+    let day = weekday[currentTime.getDay()];
+    today.innerText = day;
 }
 
 getCurrentDay();
 
 const getCurrentTime = () => {
-  var months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
-  ];
-  var now = new Date();
-  var month = months[now.getMonth()];
-  var date = now.getDate();
-  today_date.innerText = `${date} ${month}`;
+    var months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
+    var now = new Date();
+    var month = months[now.getMonth()];
+    var date = now.getDate();
+    today_date.innerText = `${date} ${month}`;
 }
 getCurrentTime();
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,32 +7,50 @@ const hbs = require("hbs");
 const staticPath = path.join(__dirname, "../public");
 const templatePath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
+const requests = require('requests');
+
 //setting view engine
 app.set('view engine', 'hbs');
-//setting views path
+//setting views pathss
 app.set("views", templatePath);
 //registering partials
 hbs.registerPartials(partialsPath)
-//serving files with express.static() middleware
+    //serving files with express.static() middleware
 app.use(express.static(staticPath));
+app.use(express.json())
+    //routing 
 
-//routing 
-
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.render("index");
 });
-app.get("/about", (req,res)=>{
+app.get("/about", (req, res) => {
     res.render("about");
 });
-app.get("/weather", (req,res)=>{
-    res.render("weather");
-})
-app.get("*", (req,res)=>{
-    res.render("404error" , {
-        errorMsg : "Oops! Page Not Found"
+app.get("/weather", (req, res) => {
+        res.render("weather");
     })
+    // api for getting weather info
+app.get("/api/weather/:cityName", (req, res) => {
+    const city = req.params.cityName;
+    let data;
+    console.log("ln  ", city)
+    requests(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`)
+        .on('data', function(chunk) {
+            data = chunk;
+            console.log(chunk)
+            console.log(data)
+            res.json(data);
+        })
+        .on('end', function(err) {
+            if (err) return console.log('connection closed due to errors', err);
+
+            console.log('end');
+        });
+
 })
-app.listen(port, ()=>{
+
+//listening to the port 
+app.listen(port, () => {
     console.log(`listening to the port ${port}`);
-    
+
 })
